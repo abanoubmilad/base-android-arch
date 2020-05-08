cc3859d3637d4f107e1bf8b5c626ff338f32888c
abanoubmilad <abanoubcs@gmail.com>
2020-05-08 15:36:45 +0200

    1.46

--- baseAndroid/src/main/java/com/me/baseAndroid/base/Isync.kt
+++ baseAndroid/src/main/java/com/me/baseAndroid/base/Isync.kt
@@ -21,6 +21,11 @@ import retrofit2.Response
  *
  */
 interface Isync {
+
+    companion object {
+        val gson = Gson()
+    }
+
     val disposable: CompositeDisposable
 
     var networkFailureCallBack: (() -> Unit)?
@@ -35,13 +40,14 @@ interface Isync {
         call: Single<R>,
         onSuccess: (R) -> Unit,
         onFailure: (Throwable) -> Unit,
-        finally: (() -> Unit)? = null
+        finally: (() -> Unit)? = null,
+        observeOnUiThread: Boolean = false
     ) {
 
         disposable.add(
             call
                 .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+                .observeOn(if (observeOnUiThread) AndroidSchedulers.mainThread() else Schedulers.io())
                 .subscribe({
                     onSuccess.invoke(it)
                     finally?.invoke()
@@ -65,14 +71,13 @@ interface Isync {
         disposable.add(
             Observable
                 .zip(
-                    firstCall.toObservable(),
-                    secondCall.toObservable(),
+                    firstCall.toObservable().subscribeOn(Schedulers.io()),
+                    secondCall.toObservable().subscribeOn(Schedulers.io()),
                     BiFunction<R, T, Pair<R, T>> { firstResponse, SecondResponse ->
                         // here we get both the results at a time.
                         return@BiFunction Pair(firstResponse, SecondResponse)
                     })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+                .observeOn(Schedulers.io())
                 .subscribe({
                     firstOnSuccess.invoke(it.first)
                     secondOnSuccess.invoke(it.second)
@@ -83,6 +88,43 @@ interface Isync {
                 })
         )
 
+    }
+
+    fun <R, T> makeNetworkRequestsSequential(
+        firstCall: Single<Response<R>>,
+        secondCall: (R?) -> Single<Response<T>>,
+        onSuccess: (T?) -> Unit,
+        onFailure: (NetworkResponseStatus) -> Unit,
+        finally: (() -> Unit)? = null
+    ) {
+
+        disposable.add(
+            firstCall
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .subscribe({ firstResponse ->
+                    handleNetworkResponse(firstResponse, { response ->
+                        disposable.add(
+                            secondCall.invoke(response)
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(Schedulers.io())
+                                .subscribe({ secondResponse ->
+                                    handleNetworkResponse(secondResponse, onSuccess, onFailure)
+                                    finally?.invoke()
+                                }, {
+                                    handleNetworkFailure(it, onFailure)
+                                    finally?.invoke()
+                                })
+                        )
+                    }, { networkResponseStatus ->
+                        onFailure.invoke(networkResponseStatus)
+                        finally?.invoke()
+                    })
+                }, {
+                    handleNetworkFailure(it, onFailure)
+                    finally?.invoke()
+                })
+        )
     }
 
     private fun handleNetworkFailure(
@@ -108,7 +150,7 @@ interface Isync {
 
             try {
                 val networkResponseStatus: NetworkResponseStatus =
-                    Gson().fromJson(
+                    gson.fromJson(
                         response.errorBody()?.charStream(),
                         NetworkResponseStatus::class.java
                     )
@@ -127,14 +169,16 @@ interface Isync {
         call: Single<Response<R>>,
         onSuccess: (R?) -> Unit,
         onFailure: (NetworkResponseStatus) -> Unit,
-        finally: (() -> Unit)? = null
+        finally: (() -> Unit)? = null,
+        observeOnUiThread: Boolean = false
     ) {
         make(call,
             {
                 handleNetworkResponse(it, onSuccess, onFailure)
             }, {
                 handleNetworkFailure(it, onFailure)
-            }, finally
+            }, finally,
+            observeOnUiThread
         )
     }
 
